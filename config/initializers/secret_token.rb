f2b61c3ae06228ced39e944846af63f13b4be209
safarista <studio@safarista.com>
2012-04-20 12:23:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bats::Application.config.secret_token = '14ca56489977cde5a657a9c2d24d96b6af1ce722326ac450706c304488ef20e0cafd9cb6bf8c8e892d184053189f8b45cf7d6886722396eef4e17b73b4246b0d'
