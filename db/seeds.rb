bcee039e28c8fe13f724e8947cb9ac2477c6b88a
Nelson Kelem <nelson@21waves.com>
2012-12-28 17:44:11 +0000

    try the photo and album slider fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,20 @@
 
 # Create the first database user with permisions to be the boss.
 
-User.create(
-  full_name: 'Nelson Kelem',
-  username: 'jackBouer',
-  email: 'nelson@safarista.com',
-  password: 'password',
-  password_confirmation: 'password',
-  admin: true,
-  about: 'I am a web and mobile applications developer based in Lincoln UK. I hail from the foothills of Narok in Kenya. Dont say anything bad about morans and Maasai, I shall take you to the bush for it. :] '
-)
+# User.create(
+#   full_name: 'Nelson Kelem',
+#   username: 'jackBouer',
+#   email: 'nelson@safarista.com',
+#   password: 'password',
+#   password_confirmation: 'password',
+#   admin: true,
+#   about: 'I am a web and mobile applications developer based in Lincoln UK. I hail from the foothills of Narok in Kenya. Dont say anything bad about morans and Maasai, I shall take you to the bush for it. :] '
+# )
+
+# Recreating all the versions of the images to reduce problems of this shit sitiing here
+# This is a carrierwave problem
+Album.all.each do |album|
+  album.photos.each do |photo|
+    photo.name.recreate_versions!
+  end
+end
